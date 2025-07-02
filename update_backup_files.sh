babcc2f84973714859cd1088cf248e71b2913c77
zaharik-ua <z0675006213@gmail.com>
2025-07-02 16:21:21 +0300

    Update update_backup_files.sh

--- update_backup_files.sh
+++ update_backup_files.sh
@@ -4,5 +4,7 @@ curl -s https://api.carcassonne.com.ua/matches -o matches.json
 git add matches.json
 curl -s https://api.carcassonne.com.ua/players -o masters.json
 git add masters.json
+curl -s https://api.carcassonne.com.ua/achievements -o achievements.json
+git add achievements.json
 git commit -m "Update json-files from server" || exit 0
 git push origin main
