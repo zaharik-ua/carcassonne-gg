8d866470ee4b4cedd6351ef494f6894d90377567
zaharik-ua <z0675006213@gmail.com>
2025-07-02 15:24:12 +0300

    Create update_matches.sh

---
+++ update_matches.sh
@@ -0,0 +1,5 @@
+cd ~/carcassonne-gg-backups
+curl -s https://api.carcassonne.com.ua/matches -o matches.json
+git add matches.json
+git commit -m "Update matches.json from server" || exit 0
+git push origin main
