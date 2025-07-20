4a6bcb3336f13ad792e1b0980d50d6b570ca51ed
Sergey <z0675006213@gmail.com>
2025-07-21 00:03:45 +0300

    updates

--- update_backup_files.sh
+++ update_backup_files.sh
@@ -11,5 +11,7 @@ curl -s https://api.carcassonne.com.ua/achievements -o achievements.json
 git add achievements.json
 curl -s https://api.carcassonne.com.ua/tournaments -o tournaments.json
 git add tournaments.json
+curl -s https://api.carcassonne.com.ua/tournaments_list -o tournaments_list.json
+git add tournaments_list.json
 git commit -m "Update json-files from server" || exit 0
 git push origin main
