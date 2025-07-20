78ac3366fa0c74e9a0748917bfbe4ba2593cddc1
Sergey <z0675006213@gmail.com>
2025-07-20 16:37:03 +0300

    added tournaments_done files. updated tournaments html file

---
+++ update_tournaments_done_files.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /home/carcassonne-gg-backups/tournaments-done || exit 1
+echo "=== $(date) ===" >> /home/carcassonne-gg-backups/cron_backup.log
+
+
+# List of tournament IDs
+tournaments=("TW-2024-CCLQ" "UCOC-2025" "HR-2025-OC" "MX-2025-CCLQ")
+
+for tournament_id in "${tournaments[@]}"; do
+  curl -s "https://api.carcassonne.com.ua/tournaments_done?tournament_id=${tournament_id}" -o "${tournament_id}.json"
+  git add "${tournament_id}.json"
+done
+
+git commit -m "Update per-tournament json files from server" || exit 0
+git push origin main
