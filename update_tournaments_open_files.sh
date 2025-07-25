07a1beaff3ed9bd302f074f019b47c109bea46bf
Sergey <z0675006213@gmail.com>
2025-07-25 19:09:42 +0300

    updates

--- update_tournaments_open_files.sh
+++ update_tournaments_open_files.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-cd /home/carcassonne-gg/json-data || exit 1
+cd /home/carcassonne-gg/json-data/tournaments-open || exit 1
 echo "=== $(date) ===" >> /home/carcassonne-gg/cron_backup.log
 
-curl -s https://api.carcassonne.com.ua/tournaments -o tournaments-open.json
-git add tournaments-open.json
+# List of tournament IDs
+tournaments=("Asian-Cup-2025" "TECS-2025" "UCOCup-2025")
+
+for tournament_id in "${tournaments[@]}"; do
+  curl -s "https://api.carcassonne.com.ua/tournaments?tournament_id=${tournament_id}" -o "${tournament_id}.json"
+  git add "${tournament_id}.json"
+done
+
 curl -s https://api.carcassonne.com.ua/tournaments_list -o tournaments-list.json
 git add tournaments-list.json
 
 git commit -m "Update json-files from server" || exit 0
 git push origin main
+
+
+
+
+
