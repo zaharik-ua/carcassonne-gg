e40147e684fe6a06c2f67af18b52c6a91944e36a
Sergey <z0675006213@gmail.com>
2025-07-25 19:26:09 +0300

    updates

--- update_tournaments_open_files.sh
+++ update_tournaments_open_files.sh
@@ -4,7 +4,7 @@ cd /home/carcassonne-gg/json-data || exit 1
 echo "=== $(date) ===" >> /home/carcassonne-gg/cron_backup.log
 
 # List of tournament IDs
-tournaments=("Asian-Cup-2025" "TECS-2025" "UCOCup-2025")
+tournaments=("Asian-Cup-2025" "TECS-2025" "UCOCup-2025" "BCPL-2025-Sum")
 
 for tournament_id in "${tournaments[@]}"; do
   curl -s "https://api.carcassonne.com.ua/tournaments?tournament_id=${tournament_id}" -o "tournaments-open/${tournament_id}.json"
