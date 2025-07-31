dd2e55bc2f994ecb16a1d7a4f48cba9e97a6f547
server <backup@carcassonne.gg>
2025-07-31 09:49:34 +0000

    Remove temp.json

--- update_backup_files.sh
+++ update_backup_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/carcassonne-gg/fallback-data || exit 1
+cd /home/carcassonne-gg/json-data || exit 1
 echo "=== $(date) ===" >> /home/carcassonne-gg/cron_backup.log
 
 curl -s https://api.carcassonne.com.ua/matches -o matches.json
@@ -9,9 +9,5 @@ curl -s https://api.carcassonne.com.ua/players -o masters.json
 git add masters.json
 curl -s https://api.carcassonne.com.ua/achievements -o achievements.json
 git add achievements.json
-curl -s https://api.carcassonne.com.ua/tournaments -o tournaments.json
-git add tournaments.json
-curl -s https://api.carcassonne.com.ua/tournaments_list -o tournaments_list.json
-git add tournaments_list.json
 git commit -m "Update json-files from server" || exit 0
 git push origin main
