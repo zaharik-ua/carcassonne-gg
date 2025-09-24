e666ac06a1439691e01efd916e229f4bb5514ad6
server <backup@carcassonne.gg>
2025-09-24 16:26:50 +0000

    update files

--- update_tournaments_done_files.sh
+++ update_tournaments_done_files.sh
@@ -7,8 +7,8 @@ echo "=== $(date) ===" >> /home/carcassonne-gg/cron_backup.log
 # List of tournament IDs
 # tournaments=("TWCOC-2024" "UCOC-2025" "HR-2025-OC")
 # tournaments=("ACOC-2025" "USCC-2025")
-# tournaments=("Asian-Cup-2025" "Copa-America-2021")
- tournaments=("TWCOC-2024")
+ tournaments=("Asian-Cup-2025")
+# tournaments=("Copa-America-2021" "BCPL-2025-Sum" "TECS-2025")
 
 for tournament_id in "${tournaments[@]}"; do
   curl -s "https://api.carcassonne.com.ua/public/tournaments_done?tournament_id=${tournament_id}" -o "${tournament_id}.json"
