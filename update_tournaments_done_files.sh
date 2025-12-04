32a06182ef1b5b66d01b2843a91003bc3ae01029
server <backup@carcassonne.gg>
2025-12-04 12:04:22 +0000

    Update all tournament scripts

--- update_tournaments_done_files.sh
+++ update_tournaments_done_files.sh
@@ -5,7 +5,7 @@ echo "=== $(date) ===" >> /home/carcassonne-gg/cron_backup.log
 
 
 # List of tournament IDs
- tournaments=("CZ-2025-COC" "CCCLQ-2025")
+ tournaments=("BCPL-2025-AUT" "BCPL-2025-AUT-1st")
 
 for tournament_id in "${tournaments[@]}"; do
   curl -s "https://api.carcassonne.com.ua/public/tournaments_done?tournament_id=${tournament_id}" -o "${tournament_id}.json"
